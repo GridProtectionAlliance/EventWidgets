54b0ba90f3719bbefa07d917ad18d86141a158d7
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2025-10-19 19:09:05 -0400

    Adapter OpenSEE controller to new format

---
+++ API/Visualizations/OpenSEE.cs
@@ -0,0 +1,61 @@
+﻿//******************************************************************************************************
+//  LineParameters.cs - Gbtc
+//
+//  Copyright © 2023, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  08/22/2023 - Preston Crawford
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+using FaultData.DataAnalysis;
+using openXDA.APIAuthentication;
+using System.Net.Http;
+
+#if IS_GEMSTONE
+using RoutePrefix = Microsoft.AspNetCore.Mvc.RouteAttribute;
+using Microsoft.AspNetCore.Mvc;
+#else
+#endif
+
+namespace Widgets.Controllers
+{
+    [RoutePrefix("api/OpenSEE")]
+    public class OpenSEEController : ControllerBase
+    {
+        #if IS_GEMSTONE
+        // if this is disabled, the static object must be intialized by the outside instead of having the function injected here
+        public OpenSEEController(Func<(string,string,string)> initFunction) 
+        {
+            if (!XDAAPIHelper.IsIntialized)
+                XDAAPIHelper.InitializeHelper(initFunction);
+        }
+        #endif
+
+        [Route("GetData"), HttpGet]
+        public Task<HttpResponseMessage> GetOpenSEEData(int eventID)
+        {
+            string? route = Request.Path.Value;
+            return XDAAPIHelper.GetResponseTask("OpenSEE/GetData");
+        }
+    }
+}
