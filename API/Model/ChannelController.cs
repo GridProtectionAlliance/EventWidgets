9d2d457e08f962f0f155a11328e6db41ee906b4e
Gabriel Santos <gcsantos@gridprotectionalliance.org>
2025-10-22 16:29:14 -0400

    addition of trend widget

---
+++ API/Model/ChannelController.cs
@@ -0,0 +1,93 @@
+﻿//******************************************************************************************************
+//  OpenSEEController.cs - Gbtc
+//
+//  Copyright © 2023, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  08/22/2023 - Preston Crawford
+//       Generated original version of source code.
+//  10/17/2025 - Gabriel Santos
+//       Removed endpoint, pointing this to an existing endpoint in XDA.
+//
+//******************************************************************************************************
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+
+#if IS_GEMSTONE
+using Gemstone.Web;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using openXDA.APIAuthentication;
+using RoutePrefix = Microsoft.AspNetCore.Mvc.RouteAttribute;
+using ServerResponse = System.Threading.Tasks.Task;
+#else
+using System.Web.Http;
+using API = openXDA.APIAuthentication.XDAAPIHelper;
+using Controller = System.Web.Http.ApiController;
+using ServerResponse = System.Threading.Tasks.Task<System.Net.Http.HttpResponseMessage>;
+#endif
+
+namespace Widgets.API.Model
+{
+    [RoutePrefix("api/EventWidgets/Channel")]
+    public class ChannelController : Controller
+    {
+        // if this is disabled, the static object must be intialized by the outside instead of having the retriever injected here
+        #if IS_GEMSTONE
+        XDAAPI API { get; set; }
+        public ChannelController(IAPICredentialRetriever retriever)
+        {
+            API = new XDAAPI(retriever);
+        }
+        #else
+        #endif
+        
+        [Route("ByParentEvent/{eventID:int}/SearchableList")]
+        [HttpPost]
+        public async ServerResponse ForwardRequest([FromBody] JObject postData, CancellationToken cancellationToken)
+        {
+            if (!API.TryRefreshSettings())
+                throw new InvalidOperationException("Unable to refresh XDA API helper.");
+
+            string endPoint;
+
+            #if IS_GEMSTONE
+            endPoint = Request.Path.Value;
+            #else
+            endPoint = Request.AbsolutePath;
+            #endif
+
+            endPoint = endPoint.Substring("api/EventWidgets/Channel".Length + 1);
+
+            StringContent content = null;
+            if (postData is not null)
+                content = new StringContent(postData.ToString(), Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await API
+                .GetResponseTask("api/Channel" + endPoint, content)
+                .ConfigureAwait(false);
+
+            #if IS_GEMSTONE
+            await Response.SetValues(response, cancellationToken);
+            return;
+            #else
+            return response;
+            #endif
+        }
+    }
+}
---
+++ TSX/Widget/TrendGraph.tsx
@@ -0,0 +1,303 @@
+﻿//******************************************************************************************************
+//  EventSearchPreviewD3Chart.tsx - Gbtc
+//
+//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  02/20/2020 - Billy Ernest
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+import { Application, HIDS, OpenXDA } from '@gpa-gemstone/application-typings';
+import { SpacedColor } from '@gpa-gemstone/helper-functions';
+import { Input } from '@gpa-gemstone/react-forms';
+import { Line, Plot, VerticalMarker } from '@gpa-gemstone/react-graph';
+import { LoadingIcon } from '@gpa-gemstone/react-interactive';
+import _ from 'lodash';
+import moment from 'moment';
+import React from 'react';
+import { EventWidget } from '../global';
+
+const sendToServerFormat = "YYYY-MM-DD[T]HH:mm:ss.SSS";
+
+interface ISetting {
+    MeasurementType: 'Current' | 'Voltage' | 'TripCoilCurrent',
+    StartHoursBefore: number,
+    EndHoursAfter: number,
+}
+
+interface IDataStructure {
+    Voltage: IInnerDataStructure,
+    Current: IInnerDataStructure,
+    TripCoilCurrent: IInnerDataStructure,
+    EventTime: number,
+    TimeLimits: [number, number]
+}
+
+interface IInnerDataStructure {
+    [key: string]: [number, number][]
+}
+
+function getColor(label) {
+    if (label.indexOf('VA') >= 0) return '#A30000';
+    if (label.indexOf('VB') >= 0) return '#0029A3';
+    if (label.indexOf('VC') >= 0) return '#007A29';
+    if (label.indexOf('VN') >= 0) return '#c3c3c3';
+    if (label.indexOf('IA') >= 0) return '#FF0000';
+    if (label.indexOf('IB') >= 0) return '#0066CC';
+    if (label.indexOf('IC') >= 0) return '#33CC33';
+    if (label.indexOf('IR') >= 0) return '#c3c3c3';
+
+    return SpacedColor(1, 0.5);
+}
+
+const TrendGraph: EventWidget.IWidget<ISetting> = {
+    Name: 'TrendGraph',
+    DefaultSettings: {
+        MeasurementType: 'Voltage',
+        StartHoursBefore: 3,
+        EndHoursAfter: 6
+    },
+    Settings: (props: EventWidget.IWidgetSettingsProps<ISetting>) => {
+        const validFunction = React.useMemo(() =>
+            (field: keyof ISetting) => {
+                if (field === 'StartHoursBefore' || field === 'EndHoursAfter')
+                    return props.Settings[field] > 0;
+                return true;
+            }
+            , [props.Settings]);
+
+        return (
+            <div className="row">
+                <div className="col">
+                    <Input<ISetting>
+                        Valid={validFunction}
+                        Record={props.Settings}
+                        Field={'StartHoursBefore'}
+                        Label={"Hours (-)"}
+                        Setter={props.SetSettings}
+                    />
+                </div>
+                <div className="col">
+                    <Input<ISetting>
+                        Valid={validFunction}
+                        Record={props.Settings}
+                        Field={'EndHoursAfter'}
+                        Label={"Hours (+)"}
+                        Setter={props.SetSettings}
+                    />
+                </div>
+            </div>);
+    },
+    Widget: (props: EventWidget.IWidgetProps<ISetting>) => {
+        const containerRef = React.useRef<HTMLTableSectionElement | undefined>(undefined);
+        const [data, setData] = React.useState<IDataStructure | undefined>(undefined);
+        const [dimensions, setDimensions] = React.useState<{ Width: number }>(null);
+        const [status, setStatus] = React.useState<Application.Types.Status>('unintiated');
+
+        React.useEffect(() => {
+            let resizeObserver: ResizeObserver;
+            const intervalHandle = setInterval(() => {
+                if (containerRef?.current == null) return;
+                resizeObserver = new ResizeObserver(
+                    _.debounce(() => {
+                        if (containerRef.current == null) return;
+
+                        // gets dims of div without rounding
+                        const dims = containerRef.current?.getBoundingClientRect();
+                        setDimensions({ Width: dims.width ?? 100 });
+                    }, 100)
+                );
+                resizeObserver.observe(containerRef.current);
+                clearInterval(intervalHandle);
+            }, 10);
+
+            return () => {
+                clearInterval(intervalHandle);
+                if (resizeObserver != null && resizeObserver.disconnect != null) resizeObserver.disconnect();
+            };
+        }, []);
+
+        React.useEffect(() => {
+            setStatus('loading');
+            let channels: OpenXDA.Types.Channel[];
+            const dataHandle: JQuery.jqXHR = $.ajax({
+                type: "POST",
+                url: `${homePath}api/EventWidgets/Channel/ByParentEvent/${props.EventID}/SearchableList`,
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(
+                    {
+                        Searches: [
+                            {
+                                FieldName: 'Trend',
+                                SearchText: '1',
+                                Operator: '=',
+                                Type: 'boolean',
+                                IsPivotColumn: false
+                            },
+                            {
+                                FieldName: 'MeasurementCharacteristic',
+                                SearchText: "RMS",
+                                Operator: '=',
+                                Type: 'string',
+                                IsPivotColumn: false
+                            },
+                            {
+                                FieldName: 'Phase',
+                                SearchText: "**N*",
+                                Operator: 'LIKE',
+                                Type: 'string',
+                                IsPivotColumn: false
+                            }],
+                        Ascending: true,
+                        OrderBy: 'ID'
+                    }),
+                dataType: 'json',
+                cache: false,
+                async: true
+            });
+            
+            dataHandle.then((chan: string) => {
+                channels = JSON.parse(chan) as OpenXDA.Types.Channel[];
+                const startTime = moment.utc(props.StartTime).subtract(props.Settings.StartHoursBefore, 'hours').format(sendToServerFormat);
+                const endTime = moment.utc(props.StartTime).add(props.Settings.EndHoursAfter, 'hours').format(sendToServerFormat);
+
+                return $.ajax({
+                    type: "POST",
+                    url: `${homePath}api/EventWidgets/HIDS/QueryPoints`,
+                    contentType: "application/json; charset=utf-8",
+                    data: JSON.stringify({
+                        Channels: channels.map(channel => channel.ID),
+                        StartTime: startTime,
+                        StopTime: endTime
+                    }),
+                    dataType: 'text',
+                    cache: false,
+                    async: true
+                });
+            }, (err) => {
+                console.error(err);
+                throw new Error(`Unable to retrieve trending data for event with id ${props.EventID}`);
+            }).then(rawData => {
+                const newPoints: string[] = rawData.split("\n");
+                const organizedData = {};
+
+                newPoints.forEach(jsonPoint => {
+                    let point: HIDS.Types.IPQData | undefined = undefined;
+                    try {
+                        if (jsonPoint !== "") point = JSON.parse(jsonPoint);
+                    }
+                    catch {
+                        console.error("Failed to parse point: " + jsonPoint);
+                    }
+                    if (point !== undefined) {
+                        const timeStamp = moment.utc(point.Timestamp, HIDS.Consts.DateTimeFormat).valueOf();
+                        // Todo: Handle alternate Series Types
+                        if (organizedData.hasOwnProperty(point.Tag)) {
+                            organizedData[point.Tag].push([timeStamp, point.Average]);
+                        } else {
+                            organizedData[point.Tag] = [[timeStamp, point.Average]];
+                        }
+                    }
+                });
+
+                const data: IDataStructure = {
+                    Voltage: {},
+                    Current: {},
+                    TripCoilCurrent: {},
+                    EventTime: moment.utc(props.StartTime).valueOf(),
+                    TimeLimits: [
+                        moment.utc(props.StartTime).subtract(props.Settings.StartHoursBefore, 'hours').valueOf(),
+                        moment.utc(props.StartTime).add(props.Settings.EndHoursAfter, 'hours').valueOf()
+                    ]
+                };
+
+                ['Voltage', 'Current', 'TripCoilCurrent'].forEach(measurementType => {
+                    const relevantChannels = channels.filter(channel => channel.MeasurementType === measurementType);
+                    const prefix = measurementType === "Voltage" ? "V" : "I";
+                    Object.keys(organizedData).forEach(tag => {
+                        const index = relevantChannels.findIndex(chan => chan.ID === Number("0x" + tag));
+                        if (index >= 0) {
+                            data[measurementType][prefix + relevantChannels[index].Phase] = organizedData[tag];
+                        }
+                    });
+                });
+                setData(data);
+                setStatus('idle');
+            }, (err) => {
+                console.error(err);
+                throw new Error(`Unable to retrieve trending data for event with id ${props.EventID}`);
+            });
+
+            return () => {
+                if (dataHandle?.abort != null) dataHandle.abort();
+            }
+        }, [props.EventID, props.Settings, props.StartTime]);
+
+        return (
+            <div className="card" ref={containerRef}>
+                <div className="card-header fixed-top" style={{ position: 'sticky', background: '#f7f7f7' }}>
+                    Trending Data
+                </div>
+                {status === 'idle' ?
+                    <div className="card-body">
+                        {Object.keys(data.Voltage).length > 0 ?
+                            <Plot height={250} width={dimensions.Width} showBorder={false}
+                                yDomain={'AutoValue'}
+                                legendWidth={150}
+                                defaultTdomain={data.TimeLimits}
+                                legend={'bottom'}
+                                Tlabel={'Time'}
+                                Ylabel={'Voltage (V)'} showMouse={false} zoom={false} pan={false} useMetricFactors={false}>
+                                <VerticalMarker Value={data.EventTime} color={'000000'} lineStyle={':'} width={3} />
+                                {Object.keys(data.Voltage).map((s) =>
+                                    <Line highlightHover={false} showPoints={false} lineStyle={'-'} color={getColor(s)} data={data.Voltage[s]} legend={s} key={s} />
+                                )}
+                            </Plot> : null}
+                        {Object.keys(data.Current).length > 0 ?
+                            <Plot height={250} width={dimensions.Width} showBorder={false}
+                                yDomain={'AutoValue'}
+                                legendWidth={150}
+                                defaultTdomain={data.TimeLimits}
+                                legend={'bottom'}
+                                Tlabel={'Time'}
+                                Ylabel={'Voltage (V)'} showMouse={false} zoom={false} pan={false} useMetricFactors={false}>
+                                <VerticalMarker Value={data.EventTime} color={'000000'} lineStyle={':'} width={3} />
+                                {Object.keys(data.Current).map((s) =>
+                                    <Line highlightHover={false} showPoints={false} lineStyle={'-'} color={getColor(s)} data={data.Current[s]} legend={s} key={s} />
+                                )}
+                            </Plot> : null}
+                        {Object.keys(data.TripCoilCurrent).length > 0 ?
+                            <Plot height={250} width={dimensions.Width} showBorder={false}
+                                yDomain={'AutoValue'}
+                                legendWidth={150}
+                                defaultTdomain={data.TimeLimits}
+                                legend={'bottom'}
+                                Tlabel={'Time'}
+                                Ylabel={'Voltage (V)'} showMouse={false} zoom={false} pan={false} useMetricFactors={false}>
+                                <VerticalMarker Value={data.EventTime} color={'000000'} lineStyle={':'} width={3} />
+                                {Object.keys(data.Voltage).map((s) =>
+                                    <Line highlightHover={false} showPoints={false} lineStyle={'-'} color={getColor(s)} data={data.TripCoilCurrent[s]} legend={s} key={s} />
+                                )}
+                            </Plot> : null}
+                    </div> : <LoadingIcon Show={true} />
+                }
+            </div>
+        );
+    }
+}
+
+export default TrendGraph;
