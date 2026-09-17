f359a24fd361ccdbc86b29bd1325ac0ed02d7bc5
prestoncraw <prestoncrawford530@gmail.com>
2026-09-17 11:37:19 -0500

    FIx layout potentially causing content to bleed into other widgets

--- TSX/Widget/ESRIMap.tsx
+++ TSX/Widget/ESRIMap.tsx
@@ -559,8 +559,8 @@ const ESRIMap: EventWidget.IWidget<ISettings> = {
         }, [faultInfo, authToken, props.Settings.TransmissionLineLayer, props.Settings.TransmissionLineQuery]);
 
         return (
-            <div className="card" style={{ maxHeight: props.MaxHeight ?? '50vh' }}>
-                <div className="card-header fixed-top" style={{ position: 'sticky' }}>
+            <div className="card" style={{ maxHeight: props.MaxHeight ?? '50vh', display: 'block', overflowY: 'auto', overflowX: 'hidden' }}>
+                <div className="card-header fixed-top" style={{ position: 'sticky', background: '#f7f7f7' }}>
                     <div className="row">
                         <div className="col-6 d-flex align-items-center">
                             ESRI Map
